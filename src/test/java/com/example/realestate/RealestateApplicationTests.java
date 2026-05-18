d08e168435a546b99b13d354813caf8e0182137b
pranathi-rishika04 <naradasupranathirishika@gmail.com>
2026-05-18 10:02:10 +0530

    Add initial test class for RealestateApplication

--- src/test/java/com/example/realestate/RealestateApplicationTests.java
+++ src/test/java/com/example/realestate/RealestateApplicationTests.java
@@ -1,1 +1,14 @@
+package com.example.realestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
 
